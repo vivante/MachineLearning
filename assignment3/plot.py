fd686e9c95822fb13908d28cd95423ddfe5c75b6
Neil Leeson Syiemlieh <neilsyiemlieh@gmail.com>
2019-03-16 14:07:15 +0530

    kmeans plot_cluster legend and figure size

--- assignment3/plot.py
+++ assignment3/plot.py
@@ -1,5 +1,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
+from matplotlib.lines import Line2D
 from matplotlib.colors import ListedColormap
 
 def plot_scatter_boundary(model, X_set, y_set, title, step_size=0.1, xlabel='x1', ylabel='x2'):
@@ -28,16 +29,32 @@ def plot_scatter_boundary(model, X_set, y_set, title, step_size=0.1, xlabel='x1'
     plt.show()
 
 def plot_clusters(cluster_model, X_set, title, xlabel='x1', ylabel='x2'):
+    '''Plot the cluster centroids,
+    their movement from their initial position to their final position
+    and the cluster assignment of the input data'''
     x1, x2 = X_set[:, 0], X_set[:, 1]
     plt.scatter(x1, x2, c=cluster_model.assignments, zorder=1)
 
-    plt.scatter(cluster_model.centroids_states[0, :, 0], cluster_model.centroids_states[0, :, 1], c='black', s=200, marker='X', zorder=3)
+    init_loc = plt.scatter(
+        cluster_model.centroids_states[0, :, 0],
+        cluster_model.centroids_states[0, :, 1],
+        c='black', s=100, marker='X', zorder=3,
+        label='Initial centroid location'
+    )
     for i in range(cluster_model.K):
         c_states = cluster_model.centroids_states[:, i]  # get all states of centroid i
-        plt.plot(c_states[:, 0], c_states[:, 1], c='cyan', linestyle='dashed', marker='.', zorder=2)
-    plt.scatter(cluster_model.centroids[:, 0], cluster_model.centroids[:, 1], c='red', s=200, marker='X', zorder=3)
+        plt.plot(c_states[:, 0], c_states[:, 1], c='cyan', linestyle='dashed', marker='.', markersize=8, zorder=2)
+    final_loc = plt.scatter(
+        cluster_model.centroids[:, 0],
+        cluster_model.centroids[:, 1],
+        c='red', s=100, marker='X', zorder=3,
+        label='Final centroid location'
+    )
+    
+    movement_path = Line2D([], [], color='cyan', marker='.', label='Centroid movement path')
 
     plt.xlabel(xlabel)
     plt.ylabel(ylabel)
     plt.title(title)
+    plt.legend(handles=[movement_path, init_loc, final_loc])
     plt.show()
